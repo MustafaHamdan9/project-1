f093ac91bf68e33ffa1afc11d51b2fcada522750
MustafaHamdan9 <mmhamdan991@gmail.com>
2021-04-23 21:21:52 +0300

    add background

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
- console.log('hello')
+ 
+ const body = document.querySelector("body");
+
+ 
+ const O = document.querySelector(".PlayerO"); 
+
+ const minibox = document.querySelectorAll(".intirebox"); 
+
+
+ const X = document.querySelector(".PlayerX"); 
+ const B1 = document.querySelector("#B1"); 
+
+ 
