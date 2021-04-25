b89b709e02bcdf2c84f59c514387cb354e263453
MustafaHamdan9 <mmhamdan991@gmail.com>
2021-04-26 00:46:37 +0300

    change the code in js code

--- main.js
+++ main.js
@@ -16,42 +16,81 @@ const b7 = document.querySelector("#B7");
 const b8 = document.querySelector("#B8");
 const b9 = document.querySelector("#B9");
 
-let jouzaTurn = true;
+let myTurn = true;
 const pressXO = (id) => {
-  if (jouzaTurn === true) {
-    document.getElementById(id).innerHTML = "x"; //  by click on pressXO start by x and then make it oppsite'false'
-    jouzaTurn = !jouzaTurn;
+  if (myTurn === true) {
+    document.getElementById(id).innerHTML = "x"; //  by click on pressXO  you will start  even by x or o and then make it oppsite'false'
+    myTurn = !myTurn;
   } else {
     document.getElementById(id).innerHTML = "o";
-    jouzaTurn = !jouzaTurn;
+    myTurn = !myTurn;
   }
+  theResult();
 };
 
 const buttonx = (id) => {
-  jouzaTurn = true;
+  myTurn = true;
 };
 
 const buttono = (id) => {
-  jouzaTurn = false;
+  myTurn = false;
 };
-//   document.getElementById(id).innerHTML = "o";
 
 const result = document.querySelector(".Result");
+
+const theResult = () => {
+  if (b1.textContent === b2.textContent && b2.textContent === b3.textContent) {
+    result.textContent = `Player ${b2.textContent} is the Winner `;
+  } else if (
+    b4.textContent === b5.textContent &&
+    b5.textContent === b6.textContent
+  ) {
+    result.textContent = `Player ${b5.textContent} is the Winner `;
+  } else if (
+    b7.textContent === b8.textContent &&
+    b8.textContent === b9.textContent
+  ) {
+    result.textContent = `Player ${b7.textContent} is the Winner `;
+  } else if (
+    b1.textContent === b4.textContent &&
+    b4.textContent === b7.textContent
+  ) {
+    result.textContent = `Player ${b7.textContent} is the Winner `;
+  } else if (
+    b2.textContent === b5.textContent &&
+    b5.textContent === b8.textContent
+  ) {
+    result.textContent = `Player ${b5.textContent} is the Winner `;
+  } else if (
+    b3.textContent === b6.textContent &&
+    b6.textContent === b9.textContent
+  ) {
+    result.textContent = `Player ${b9.textContent} is the Winner `;
+  } else if (
+    b1.textContent === b5.textContent &&
+    b5.textContent === b9.textContent
+  ) {
+    result.textContent = `Player ${b5.textContent} is the Winner `;
+  } else if (
+    b3.textContent === b5.textContent &&
+    b5.textContent === b7.textContent
+  ) {
+    result.textContent = `Player ${b7.textContent} is the Winner `;
+  } else {
+    result.textContent = "Draw";
+  }
+};
 
 const rstbot = document.querySelector(".button");
 
 const resetClick = () => {
   // itterate over boxs(1-9)
   // change every box value to ''
-  //
-  // rerer
 
   const a = [b1, b2, b3, b4, b5, b6, b7, b8, b9, result];
-  re = a.map(function (e, i) {
+  reset = a.map(function (e, i) {
     e.innerHTML = "";
     return;
   });
-  return re;
+  return reset;
 };
-
-// return b1.innerHTML = '',b2.innerHTML = '' ,b3.innerHTML = '',b4.innerHTML = '',b5.innerHTML = '',b6.innerHTML = '',b7.innerHTML = '',b8.innerHTML = '',b9.innerHTML = '',result.innerHTML=''
